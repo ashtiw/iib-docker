ebdbfbcc1af5f1aafb31ff1d39532a3e0780eafa
Ashish Tiwari <ashish.tiwari@anz.com>
2016-01-19 13:44:19 +0000

    Added test comment

--- iib_env.sh
+++ iib_env.sh
@@ -5,6 +5,7 @@
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
+# Just a test comment
 
 if [ -z "$MQSI_VERSION" ]; then
   echo "Sourcing profile"
